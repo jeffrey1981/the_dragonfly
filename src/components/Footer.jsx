38a874d45dcea5f56a121ac1ad087de3c8c698f3
jeffrey1981 <jeffersonslewis@gmail.com>
2025-02-03 17:55:50 -0800

    footer css update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
-      <p>2025 Bugs and Stuff</p>
+    <footer className="footer">
+      <div className="footer-content">
+        <p>2025 Bugs and Stuff</p>
+        <nav className="footer-nav">
+          <a href="#">Contact Us</a>
+          <a href="#">About Us</a>
+        </nav>
+      </div>
+      <img
+        src="https://images.unsplash.com/photo-1597040827713-24d4c7e4b0e2?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Z3JlZW4lMjBncmFzc3xlbnwwfHwwfHx8MA%3D%3D"
+        alt=""
+        className="footer-image"
+      />
     </footer>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header>
-      <h1>Insects Native to California</h1>
+    <header className="header">
+      <img
+        src="https://animals.sandiegozoo.org/sites/default/files/2016-10/animals_hero_dragonfly.jpg"
+        alt="Dragonfly"
+        className="header-image"
+      />
+      <h1 className="header-title">The Amazing World of Dragonflies</h1>
     </header>
   )
 }
